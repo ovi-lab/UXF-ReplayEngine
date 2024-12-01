f0a725506e3336b5bb9d9f778952fe73181eacb9
omangbaheti <omangbaheti@gmail.com>
2024-11-30 17:37:29 -0800

    Adding Icons for REplay Engine Functions

--- Editor/ReplayEngineCustomEditor.cs
+++ Editor/ReplayEngineCustomEditor.cs
@@ -1,55 +1,86 @@
-using ubco.ovilab.uxf.replayengine;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(ReplayEngine))]
-public class ReplayEngineCustomEditor : Editor
+
+
+namespace ubco.ovilab.uxf.replayengine
 {
-    public override void OnInspectorGUI()
+    [CustomEditor(typeof(ReplayEngine))]
+    public class ReplayEngineCustomEditor : Editor
     {
-        ReplayEngine replayEngine = (ReplayEngine)target;
 
-        if (GUILayout.Button("Select Path and Load Data"))
+        private Texture2D playTrialIcon;
+        private Texture2D restartTrialIcon;
+        private Texture2D pauseTrialIcon;
+        private Texture2D nextTrialIcon;
+        private Texture2D previousTrialIcon;
+        private GUIStyle buttonStyle;
+        private void OnEnable()
         {
-            string path = EditorUtility.OpenFolderPanel("Select Folder", "", "");
-            if(!string.IsNullOrEmpty(path))
-                replayEngine.LoadData(path, replayEngine.ParticipantID, replayEngine.Session, replayEngine.TrialsFile);
-            else
+            playTrialIcon = (Texture2D)AssetDatabase.LoadAssetAtPath("Packages/ubco.ovilab.uxf.replay-engine/Editor/Icons/play.png", typeof(Texture2D));
+            restartTrialIcon = (Texture2D)AssetDatabase.LoadAssetAtPath("Packages/ubco.ovilab.uxf.replay-engine/Editor/Icons/restart.png", typeof(Texture2D));
+            pauseTrialIcon = (Texture2D)AssetDatabase.LoadAssetAtPath("Packages/ubco.ovilab.uxf.replay-engine/Editor/Icons/pause.png", typeof(Texture2D));
+            previousTrialIcon = (Texture2D)AssetDatabase.LoadAssetAtPath("Packages/ubco.ovilab.uxf.replay-engine/Editor/Icons/previous.png", typeof(Texture2D));
+            nextTrialIcon = (Texture2D)AssetDatabase.LoadAssetAtPath("Packages/ubco.ovilab.uxf.replay-engine/Editor/Icons/next.png", typeof(Texture2D));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            ReplayEngine replayEngine = (ReplayEngine)target;
+            
+            buttonStyle = new GUIStyle(GUI.skin.button)
+            { 
+                fixedWidth = 50,  // Button width
+                fixedHeight = 50,  // Button height
+            };
+
+            if (GUILayout.Button("Select Path and Load Data"))
             {
-                Debug.LogError("Something went wrong, try again!");
+                string path = EditorUtility.OpenFolderPanel("Select Folder", "", "");
+                if(!string.IsNullOrEmpty(path))
+                    replayEngine.LoadData(path, replayEngine.ParticipantID, replayEngine.Session, replayEngine.TrialsFile);
+                else
+                {
+                    Debug.LogError("Something went wrong, try again!");
+                }
             }
-        }
 
-        if (GUILayout.Button("Set Active Tracker Replayers"))
-        {
-            replayEngine.SetActiveTrackerReplayers(replayEngine.TargetActiveTrackerReplayers);
-        }
+            if (GUILayout.Button("Set Active Tracker Replayers"))
+            {
+                replayEngine.SetActiveTrackerReplayers(replayEngine.TargetActiveTrackerReplayers);
+            }
 
-        if (GUILayout.Button("Next Trial"))
-        {
-            replayEngine.LoadNextTrial();
-        }
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+                if (GUILayout.Button(new GUIContent(previousTrialIcon), buttonStyle))
+                {
+                    replayEngine.LoadPrevTrial();
+                }
+                
+                if (GUILayout.Button(new GUIContent(restartTrialIcon), buttonStyle))
+                {
+                    replayEngine.Restart();
+                }
 
-        if (GUILayout.Button("Previous Trial"))
-        {
-            replayEngine.LoadPrevTrial();
-        }
+                if (GUILayout.Button(new GUIContent(playTrialIcon), buttonStyle))
+                {
+                    replayEngine.Play();
+                }
 
-        if (GUILayout.Button("Restart"))
-        {
-            replayEngine.Restart();
-        }
+                if (GUILayout.Button(new GUIContent(pauseTrialIcon), buttonStyle))
+                {
+                    replayEngine.Pause();
+                }
+                
+                if (GUILayout.Button(new GUIContent(nextTrialIcon), buttonStyle))
+                {
+                    replayEngine.LoadNextTrial();
+                }
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
 
-        if (GUILayout.Button("Play"))
-        {
-            replayEngine.Play();
+            DrawDefaultInspector();
         }
-
-        if (GUILayout.Button("Pause"))
-        {
-            replayEngine.Pause();
-        }
-
-        DrawDefaultInspector();
     }
 }
+
